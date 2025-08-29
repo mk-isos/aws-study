e2cfc227ce8c08e4aa6fab139c7d6221cf2a4a69
mk-isos <mkisos7900@naver.com>
2025-08-29 09:25:29 +0900

    250829 Add aws-final-springboot source code

---
+++ aws-final-springboot/src/main/java/com/example/awsfinalspringboot/AwsFinalSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsfinalspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsFinalSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsFinalSpringbootApplication.class, args);
+	}
+
+}
